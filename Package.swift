f7e5cde61583dcb68ad8be39d9dba7b3718e9667
go <go.ko@healingpaper.com>
2020-06-24 16:53:21 +0900

    add SPM support

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Gherkin",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Gherkin",
+            targets: ["Gherkin"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "8.1.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Gherkin",
+            dependencies: ["Quick", "Nimble"]),
+        .testTarget(
+            name: "GherkinTests",
+            dependencies: ["Gherkin"]),
+    ]
+)
